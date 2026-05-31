eac19df750dd7fa2b6e3489f3dc2dbd2bf4059cc
IgorP17 <iprokhorovster@gmail.com>
2026-05-31 10:15:33 +0300

    run_me - do not package

--- brahma-gateway/run_me.sh
+++ brahma-gateway/run_me.sh
@@ -1,4 +1,23 @@
-export MAVEN_OPTS="-Xms256m -Xmx512m -XX:+UseG1GC"
-# mvn quarkus:dev
-mvn clean package
-java -jar target/quarkus-app/quarkus-run.jar
+#!/usr/bin/env bash
+
+# Опции для Maven (только для сборки, не влияет на запуск JAR)
+#export MAVEN_OPTS="-Xms256m -Xmx512m -XX:+UseG1GC"
+
+# Сборка всего проекта (включая brahma-common, brahma-gateway, brahma-processor, brahma-webui)
+# Это гарантирует, что brahma-common установлен в ~/.m2/repository
+# mvn clean install
+# mvn clean package
+
+# Проверка, успешна ли сборка
+#if [ $? -ne 0 ]; then
+#  echo "Build failed!"
+#  exit 1
+#fi
+
+# Опции для запуска JAR (влияют на JVM приложения brahma-processor)
+JAVA_OPTS="-Xms256m -Xmx512m -XX:+UseG1GC"
+
+# Запуск JAR brahma-processor
+java $JAVA_OPTS -jar target/quarkus-app/quarkus-run.jar
+#java -Dquarkus.log.level=DEBUG -Dquarkus.log.category."io.quarkus.grpc".level=DEBUG -jar target/quarkus-app/quarkus-run.jar
+## mvn quarkus:dev
--- brahma-webui/run_me.sh
+++ brahma-webui/run_me.sh
@@ -1,4 +1,5 @@
-export MAVEN_OPTS="-Xms256m -Xmx512m -XX:+UseG1GC"
-# mvn quarkus:dev
-mvn clean package
-java -jar target/quarkus-app/quarkus-run.jar
+#!/usr/bin/env bash
+
+#export MAVEN_OPTS="-Xms256m -Xmx512m -XX:+UseG1GC"
+JAVA_OPTS="-Xms256m -Xmx512m -XX:+UseG1GC"
+java $JAVA_OPTS -jar target/quarkus-app/quarkus-run.jar
